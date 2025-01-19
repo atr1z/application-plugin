af77e43d5f1d02d3564abc95ccdbaf8ac61de570
Atriz <dev@atriz.com.mx>
2025-01-19 13:08:09 -0600

    version fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.vanniktech.maven.publish.GradlePublishPlugin
 import com.vanniktech.maven.publish.SonatypeHost
 
 group = "mx.com.atriz"
-version = "0.0.8"
+version = "0.0.9"
 
 plugins {
     kotlin("jvm") version "2.0.21"
--- src/main/kotlin/mx/com/atriz/core/Version.kt
+++ src/main/kotlin/mx/com/atriz/core/Version.kt
@@ -3,9 +3,9 @@ package mx.com.atriz.core
 import org.gradle.api.JavaVersion
 
 object Version {
-    const val COMPILE_SDK = 34
+    const val COMPILE_SDK = 35
     const val MIN_SDK = 30
-    const val TARGET_SDK = 34
+    const val TARGET_SDK = 35
 
     fun java(): JavaVersion = JavaVersion.VERSION_21
 }
