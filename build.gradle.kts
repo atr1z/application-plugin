00f5300989f26c65b8aff7d5a57d0c8a5a60db59
Jair <atriz@developer.local>
2025-01-27 09:51:12 -0600

    fix on github actions bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.vanniktech.maven.publish.GradlePublishPlugin
 import com.vanniktech.maven.publish.SonatypeHost
 
 group = "mx.com.atriz"
-version = "0.0.9"
+version = "0.1.0"
 
 plugins {
     kotlin("jvm") version "2.0.21"
